bee0b734636e6283aaf361a1da6987dbe973b39a
Wende Tan <twd2@163.com>
2017-10-17 22:14:07 +0800

    fix controller

--- ex_alu/controller.vhd
+++ ex_alu/controller.vhd
@@ -1,21 +1,18 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
-use work.signals.all;
-use work.state.all;
-use work.image_info.all;
 use work.constants.all;
 
-entity wow_crow is
+entity controller is
     port
     (
-        CLK: in std_logic; 
-        RST: in std_logic;
-        InputSW: in std_logic_vector(15 downto 0);
-        fout: out std_logic_vector(15 downto 0);
+        nCLK: in std_logic; 
+        nRST: in std_logic;
+        nInputSW: in word_t;
+        fout: out word_t
     );
 end;
 
-architecture behavioral of wow_crow is
+architecture behavioral of controller is
     component alu is 
         port
         (
@@ -34,34 +31,44 @@ architecture behavioral of wow_crow is
             CARRY: out std_logic
         );
     end component;
+
     type state is (s_inputA, s_inputB, s_inputOP, s_outputFout, s_outputFlag);
-    signal current_state : state := s_inputA;
+    signal current_state: state;
+    
+    signal clk, rst: std_logic;
+    signal InputSW: word_t;
+
     signal OP: alu_op_t;
     signal OPERAND_0: word_t;
     signal OPERAND_1: word_t;
     signal result: word_t;
     signal output: word_t;
-    signal OVERFLOW: out std_logic;
-    signal ZERO: out std_logic;
-    signal SIGN: out std_logic;
-    signal CARRY: out std_logic;
+    signal OVERFLOW: std_logic;
+    signal ZERO: std_logic;
+    signal SIGN: std_logic;
+    signal CARRY: std_logic;
 begin
+    clk <= not nCLK;
+    rst <= not nRST;
+    InputSW <= not nInputSW;
+
     fout <= output;
+
     alu_inst: alu
     port map
     (
         -- input
-        OP => OP;
-        OPERAND_0 => OPERAND_0;
-        OPERAND_1 => OPERAND_1;
+        OP => OP,
+        OPERAND_0 => OPERAND_0,
+        OPERAND_1 => OPERAND_1,
 
         -- output
-        RESULT => result;
+        RESULT => result,
 
         -- flags
-        OVERFLOW => OVERFLOW;
-        ZERO => ZERO;
-        SIGN => SIGN;
+        OVERFLOW => OVERFLOW,
+        ZERO => ZERO,
+        SIGN => SIGN,
         CARRY => CARRY
     );
 
@@ -69,28 +76,41 @@ begin
     begin
         if RST = '1' then
             current_state <= s_inputA;
-            OPERAND_0 <= InputSW;
         elsif rising_edge(CLK) then
             case current_state is
                 when s_inputA =>
+                    OPERAND_0 <= InputSW;
                     current_state <= s_inputB;
-                    OPERAND_1 <= InputSW;                   
                 when s_inputB =>
+                    OPERAND_1 <= InputSW;   
                     current_state <= s_inputOP;
-                    OP <= InputSW[3 downto 0]; 
                 when s_inputOP =>
-                    current_state <= s_outFout;
-                    output <= result;                     
-                when s_outFout =>
-                    current_state <= s_outFlag;
-                    output[0] <= OVERFLOW;
-                    output[1] <= ZERO;
-                    output[2] <= SIGN;
-                    output[3] <= CARRY;                      
+                    OP <= InputSW(3 downto 0);
+                    current_state <= s_outputFout;
+                when s_outputFout =>
+                    current_state <= s_outputFlag;
+                when s_outputFlag =>
+                    OPERAND_0 <= InputSW;
+                    current_state <= s_inputB;
                 when others =>
                     current_state <= s_inputA;
-                    OPERAND_0 <= InputSW;
             end case;
         end if;
+    end process;
+
+    output_proc : process(current_state, result, OVERFLOW, ZERO, SIGN, CARRY)
+    begin
+        case current_state is
+            when s_outputFout =>
+                output <= result;
+            when s_outputFlag =>
+                output <= (others => '0');
+                output(0) <= OVERFLOW;
+                output(1) <= ZERO;
+                output(2) <= SIGN;
+                output(3) <= CARRY;
+            when others =>
+                output <= (others => '0');
+        end case;
     end process;
 end;
