4f8837cb7d0462730dda44f483b75e97baf71502
刘明华 <liumh413@gmail.com>
2017-12-02 01:26:33 +0800

    add 2048_block_name_gen.py

---
+++ as/2048_block_name_gen.py
@@ -0,0 +1,10 @@
+blocks = ['Colin','bill125','lazycal','twd2','fsygd','wuhz','YYF','128','256','512','1024','lss']
+for block in blocks:
+	l = len(block)
+	ll = (12 - l) / 2
+	for i in range(ll):
+		print '.word 32'
+	for char in block:
+		print '.word \'' + char + '\''
+	for i in range(12 - l -ll):
+		print '.word 32'
