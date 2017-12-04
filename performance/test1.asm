70863c7a44d1e96d5b60b0fa939e6beb6424f294
Wende Tan <twd2@163.com>
2017-12-04 12:27:20 +0800

    add performance test result

---
+++ performance/test1.asm
@@ -0,0 +1,39 @@
+; 性能标定
+; 这段程序一般没有数据冲突和结构冲突，可作为性能标定。
+; 结果：
+; 6.006s @ 25.0000MHz, CPI=1.00
+; failed @ 100.000MHz, CPI=----
+
+; *** 程序说明：R4、R5为循环变量   ***
+; ***     主要循环体0x0D~0x12，6条 ***
+; ***     每条各执行25,000,000次   ***
+; ***     共1.50亿条指令           ***
+; ***     （行号从0开始）          ***
+
+LI R5 FF
+NOP
+NOP
+NOP
+SLL R5 R5 0
+NOP
+NOP
+NOP
+ADDIU R5 82
+LI R4 60
+NOP
+NOP
+NOP
+ADDIU R4 1
+LI R0 0
+LI R1 1
+LI R2 2
+BNEZ R4 FB
+NOP
+ADDIU R5 1
+NOP
+NOP
+NOP
+BNEZ R5 F1
+NOP
+JR R7
+NOP
---
+++ performance/test6.asm
@@ -0,0 +1,29 @@
+; 访存数据冲突+控制冲突性能测试
+
+; 结果：
+; ??s @ ??.0000MHz, CPI=?.??
+; failed @ 100.000MHz, CPI=----
+
+; *** 程序说明：R4、R5为循环变量   ***
+; ***     主要循环体0x07~0x0B，5条 ***
+; ***     每条各执行25,000,000次   ***
+; ***     共1.25亿条指令           ***
+
+; LW 后紧跟 BNEZ。
+
+LI R2 FF
+LI R3 C0
+SLL R3 R3 0
+LI R5 FF
+SLL R5 R5 0
+ADDIU R5 83
+LI R1 61
+SW R3 R1 2
+SW R3 R5 1
+LW R3 R4 2
+BNEZ R4 FC
+ADDIU R1 1
+BNEZ R5 F9
+ADDIU R5 1
+JR R7
+NOP
