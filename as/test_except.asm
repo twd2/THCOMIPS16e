2b857696cb26a96046aeeca243ba0efa69ef8fc0
Wende Tan <twd2@163.com>
2017-12-07 16:53:39 +0800

    test exceuption

--- as/test_except.asm
+++ as/test_except.asm
@@ -3,6 +3,8 @@ nop
 
 .org 0x10
 ; internal interrupt handler
+syscall ; nop
+mfpc r0
 mfc0 r0, cause
 mfc0 r1, epc
 addiu r1, 1 ; skip syscall
@@ -11,14 +13,18 @@ eret
 
 .org 0x20
 ; external interrupt handler
+syscall ; nop
+mfpc r0
 mfc0 r0, cause
 mfc0 r1, epc
 eret
 
 main:
+    li r0, 0x77
+    syscall 777
     li r0, 0x0001
     mtc0 r0, status ; enable exception
-    li r0, 0x11
+    li r0, 0x66
     syscall 666
     li r0, 0x22
 
