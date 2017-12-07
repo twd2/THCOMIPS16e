72733df2a7e4f9c63ea7e7bfe88703d516efd4e7
Wende Tan <twd2@163.com>
2017-12-07 12:42:33 +0800

    support quit

--- as/badapple.asm
+++ as/badapple.asm
@@ -11,9 +11,9 @@ badapple:
     swsp r6, 6
     swsp r7, 7
     li r0, 0 ; frame counter
-    la r4, vga_control_base
-    sw r4, r0, 3 ; turn off cursor
-    la r4, sd_base ; SD controller base
+    la r5, vga_control_base
+    sw r5, r0, 3 ; turn off cursor
+    la r5, sd_base ; SD controller base
 _badapple_play_loop:
     li r1, 6560 ; 6560 frames totally
     cmp r0, r1
@@ -22,34 +22,46 @@ _badapple_play_loop:
     ; load current frame from SD card
     ; 16 sectors per frame
     sll r1, r0, 4 ; r1 = (r0 * 16) % 65536
-    sw r4, r1, 0 ; sector_base
+    sw r5, r1, 0 ; sector_base
     sra r1, r0, 8
     sra r1, r1, 4 
     li r2, 0xf
     and r1, r2 ; r1 = (r0 * 16) / 65536 = r0 / 4096
     addiu r1, 1 ; 64K sectors (32MiB) offset
-    sw r4, r1, 1 ; sector_base hi
+    sw r5, r1, 1 ; sector_base hi
     la r1, badapple_buffer
-    sw r4, r1, 2 ; ram_base
+    sw r5, r1, 2 ; ram_base
     li r1, 0
-    sw r4, r1, 3 ; ram_base hi
+    sw r5, r1, 3 ; ram_base hi
     li r1, 16 ; 4K words = 16 sectors
-    sw r4, r1, 4 ; sector_count
+    sw r5, r1, 4 ; sector_count
     li r1, 0
-    sw r4, r1, 5 ; sector_count hi
+    sw r5, r1, 5 ; sector_count hi
     li r1, 1 ; command: read
-    sw r4, r1, 7 ; start command
+    sw r5, r1, 7 ; start command
     
     ; wait for SD controller
     li r2, 0x0001 ; interrupt bit
     _badapple_wait_irq:
-        lw r4, r1, 6
+        lw r5, r1, 6
         move r3, r1
         and r3, r2
-        ; TODO: check PS/2 here to quit
+        ; check PS/2 here to quit
+        la r6, ps2_base
+        lw r6, r6, 1
+        li r7, 0x0001
+        and r6, r7
+        beqz r6, _badapple_wait_continue
+        nop
+        call getchar
+        nop
+        cmpi r4, 'q'
+        bteqz _badapple_done
+        nop
+    _badapple_wait_continue:
         beqz r3, _badapple_wait_irq
         nop
-    sw r4, r1, 6 ; clear interrupt bit
+    sw r5, r1, 6 ; clear interrupt bit
     
     ; copy badapple_buffer to graphics_base
     la r1, badapple_buffer
