fc2e9c8bb7ee39b92c05331b083afc33bf99f126
刘明华 <liumh413@gmail.com>
2017-12-08 09:22:16 +0800

    grumble

--- as/danmuku.asm
+++ as/danmuku.asm
@@ -1,12 +1,12 @@
 _danmuku:
-	la r0, danmuku_read_done
-	lw r0, r0, 0
-	cmpi r0, 1 ; read_done = 1
-	bteqz _damuku_draw
-	nop
+    la r0, danmuku_read_done
+    lw r0, r0, 0
+    cmpi r0, 1 ; read_done = 1
+    bteqz _damuku_draw
+    nop
 
 _danmuku_gets:
-	la r0, ps2_base
+    la r0, ps2_base
     lw r0, r0, 1
     li r1, 0x0001
     and r0, r1
@@ -28,63 +28,63 @@ _danmuku_gets:
     nop
 
 _danmuku_read_done:
-	la r0, danmuku_read_done	
-	li r1, 1
-	sw r0, r1, 0 ; read_done = 1
-	la r0, danmuku_cnt	
-	li r1, 0
-	sw r0, r1, 0 ; cnt = 0
-	la r0, danmuku_pos	
-	li r1, 0
-	sw r0, r1, 0 ; pos = 0
-	b _danmuku_done
-	nop
+    la r0, danmuku_read_done    
+    li r1, 1
+    sw r0, r1, 0 ; read_done = 1
+    la r0, danmuku_cnt    
+    li r1, 0
+    sw r0, r1, 0 ; cnt = 0
+    la r0, danmuku_pos    
+    li r1, 0
+    sw r0, r1, 0 ; pos = 0
+    b _danmuku_done
+    nop
 
 _danmuku_draw:
-	la r0, danmuku_cnt
-	lw r0, r1, 0 ; r1 cnt
-	addiu r1, 1 ; cnt++
-	sw r0, r1, 0 ; save cnt
-	cmpi r1, 30 ; cnt == 30
-	bteqz _danmuku_new_pos
-	nop
-	la r0, danmuku_addr
-	la r1, danmuku_pos
-	li r2, 0x0700 ; color
-	li r3, 0
-	_danmuku_draw_loop:
-		addu r0, r3, r4
-		lw r4, r4, 0 ; char
-		cmpi r4, 10
-		bteqz _danmuku_done
-		nop
-		or r4, r2
-		la r5, graphics_base
-		addiu r5, 160 ; 2 * 80
-		addu r5, r1, r5 ; += pos
-		addu r5, r3, r5
-		sw r5, r4, 0
-		addiu r3, 1
-		b _danmuku_draw_loop
-		nop
+    la r0, danmuku_cnt
+    lw r0, r1, 0 ; r1 cnt
+    addiu r1, 1 ; cnt++
+    sw r0, r1, 0 ; save cnt
+    cmpi r1, 30 ; cnt == 30
+    bteqz _danmuku_new_pos
+    nop
+    la r0, danmuku_addr
+    la r1, danmuku_pos
+    li r2, 0x0700 ; color
+    li r3, 0
+    _danmuku_draw_loop:
+        addu r0, r3, r4
+        lw r4, r4, 0 ; char
+        cmpi r4, 10
+        bteqz _danmuku_done
+        nop
+        or r4, r2
+        la r5, graphics_base
+        addiu r5, 160 ; 2 * 80
+        addu r5, r1, r5 ; += pos
+        addu r5, r3, r5
+        sw r5, r4, 0
+        addiu r3, 1
+        b _danmuku_draw_loop
+        nop
 
 _danmuku_new_pos:
-	la r0, danmuku_pos
-	lw r0, r1, 0 ; r1 pos
-	addiu r1, 1 ; pos++
-	sw r0, r1, 0 ; save pos
-	cmpi r1, 70
-	bteqz _danmuku_new_danmu
-	nop
-	b _danmuku_done
-	nop
+    la r0, danmuku_pos
+    lw r0, r1, 0 ; r1 pos
+    addiu r1, 1 ; pos++
+    sw r0, r1, 0 ; save pos
+    cmpi r1, 70
+    bteqz _danmuku_new_danmu
+    nop
+    b _danmuku_done
+    nop
 
 _danmuku_new_danmu:
-	la r0, danmuku_read_done	
-	li r1, 0
-	sw r0, r1, 0 ; read_done = 0
-	la r0, danmuku_read_cnt	
-	sw r0, r1, 0 ; cnt = 0
+    la r0, danmuku_read_done    
+    li r1, 0
+    sw r0, r1, 0 ; read_done = 0
+    la r0, danmuku_read_cnt    
+    sw r0, r1, 0 ; cnt = 0
 
 _danmuku_done:
     lwsp r0, 0
